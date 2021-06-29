7705ac0988432ac3ce5aed88e4bc3a0d4bc22007
wangyunbo <wangyunbo@hatech.com.cn>
2021-06-29 17:55:08 +0800

    JTable component

---
+++ components/JTable/JTable.stories.js
@@ -0,0 +1,34 @@
+/*
+ * @Author: wangyunbo
+ * @Date: 2021-06-29 17:53:37
+ * @LastEditors: wangyunbo
+ * @LastEditTime: 2021-06-29 17:54:06
+ * @Description: file content
+ * @FilePath: \medium-reusable-vue-components\components\JTable\JTable.stories.js
+ */
+import { storiesOf } from '@storybook/vue'
+import JTable from './JTable'
+
+var items = [
+  [ 'Yair Lamb', 'yair.lamb@email.com' ],
+  [ 'Leonardo Payne', 'leonardo.payne@email.com' ],
+  [ 'Carl Henson', 'carl.henson@email.com' ],
+  [ 'Jensen Combs', 'jensen.combs@email.com' ],
+  [ 'Amiah Burton', 'amiah.burton@email.com' ],
+  [ 'Yaretzi Mayo', 'yaretzi.mayo@email.com' ],
+  [ 'Kamren Huffman', 'kamren.huffman@email.com' ]
+]
+
+storiesOf('JTable', module)
+  .add('with 7 items', () => ({
+    components: { JTable },
+    template:
+    `
+      <JTable
+        :items="items"
+      />
+    `,
+    data: () => ({
+      items
+    })
+  }))
---
+++ components/JTable/JTable.vue
@@ -0,0 +1,33 @@
+<!--
+ * @Author: wangyunbo
+ * @Date: 2021-06-29 17:52:06
+ * @LastEditors: wangyunbo
+ * @LastEditTime: 2021-06-29 17:53:21
+ * @Description: file content
+ * @FilePath: \medium-reusable-vue-components\components\JTable\JTable.vue
+-->
+<template>
+  <table>
+    <JTableRow
+      v-for="(values, index) in items"
+      :key="index"
+      :values="values"
+    />
+  </table>
+</template>
+
+<script>
+import JTableRow from '@airbender92/j-table-row/JTableRow'
+export default {
+  name: 'JTable',
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  components: {
+    JTableRow
+  }
+}
+</script>
